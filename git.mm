be59b65d5f7a49afa3b5ac71a0bc6d2525dbada3
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-08-30 14:44:56 +0200

    dodano git.mm

---
+++ git.mm
@@ -0,0 +1,18 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1346316450419" ID="ID_644194117" MODIFIED="1346316536873" TEXT="git">
+<node CREATED="1346316542529" ID="ID_1607883105" MODIFIED="1346316611047" POSITION="right" TEXT="1. PierwszeKroki">
+<node CREATED="1346316617875" ID="ID_670157224" MODIFIED="1346316653051" TEXT="1.1 KontrolaWersji">
+<node CREATED="1346316655332" ID="ID_872864042" MODIFIED="1346316666611" TEXT="&#x15b;ledzenie zmian plik&#xf3;w"/>
+<node CREATED="1346316667378" ID="ID_755525776" MODIFIED="1346316676087" TEXT="umo&#x17c;liwienie cofni&#x119;cia si&#x119; do dowolnego etapu zmian"/>
+<node CREATED="1346317326713" ID="ID_1402056145" MODIFIED="1346317450215" TEXT="LokalnySystemKontroliWersji">
+<node CREATED="1346317340324" ID="ID_693257133" MODIFIED="1346317349698" TEXT="katalogowane kopie na lokalnym dysku"/>
+<node CREATED="1346317369128" ID="ID_74766253" MODIFIED="1346317370410" TEXT="rcs">
+<node CREATED="1346317373241" ID="ID_1637004717" MODIFIED="1346317400575" TEXT="przechowywal tylko dane roznicowe (same zmiany, pomiedzy plikami)"/>
+</node>
+</node>
+<node CREATED="1346317424168" ID="ID_406733052" MODIFIED="1346317441003" TEXT="ScentralizowanySystemKontroliWersji"/>
+</node>
+</node>
+</node>
+</map>
