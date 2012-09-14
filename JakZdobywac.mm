a5b0909be370e775274d1e249ae3223567a74537
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-09-14 13:43:33 +0200

    Nowa mapa - JakZdobywac

---
+++ JakZdobywac.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1347622949358" ID="ID_482084534" MODIFIED="1347622955384" TEXT="Jak zdobywa&#x107;"/>
+</map>
