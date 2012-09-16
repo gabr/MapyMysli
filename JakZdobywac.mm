6993957910fd5883fac3dda6c171b61ebf9e46db
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-09-16 18:32:11 +0200

    Dodanie gałęzi w JakZdobywac

--- JakZdobywac.mm
+++ JakZdobywac.mm
@@ -8,6 +8,59 @@
 <node CREATED="1347638540069" ID="ID_1456820101" MODIFIED="1347638556952" TEXT="najcz&#x119;&#x15b;ciej to co najbardziej je poci&#x105;ga, jednocze&#x15b;nie je denerwuje"/>
 <node CREATED="1347638570885" ID="ID_1280892898" MODIFIED="1347638605372" TEXT="tu nie chodzi o d&#x105;&#x17c;enie do tego by ci&#x119; lubia&#x142;a - lubi si&#x119; przyjaci&#xf3;&#x142;"/>
 </node>
-<node CREATED="1347638614165" ID="ID_300467335" MODIFIED="1347638617636" POSITION="right" TEXT="2. Wprowadzenie"/>
+<node CREATED="1347638614165" ID="ID_300467335" MODIFIED="1347638617636" POSITION="right" TEXT="2. Wprowadzenie">
+<node CREATED="1347801514351" ID="ID_1314482375" MODIFIED="1347801519948" TEXT="dobre i z&#x142;e uwodzenie">
+<node CREATED="1347809305378" ID="ID_1557103882" MODIFIED="1347809312857" TEXT="przedmiotowe traktowanie"/>
+<node CREATED="1347809318693" ID="ID_1059166014" MODIFIED="1347809321402" TEXT="lepsze zrozumienie"/>
+<node CREATED="1347809322048" ID="ID_1102104170" MODIFIED="1347809330083" TEXT="udane zwi&#x105;zki"/>
+<node CREATED="1347809330339" ID="ID_98916441" MODIFIED="1347809345772" TEXT="z&#x142;amane serca"/>
+<node CREATED="1347809353225" ID="ID_1889833402" MODIFIED="1347809357469" TEXT="mo&#x17c;e sta&#x107; si&#x119; na&#x142;ogiem"/>
+<node CREATED="1347809338013" ID="ID_774959143" MODIFIED="1347809341838" TEXT="rozw&#xf3;j osobisty"/>
+<node CREATED="1347809385963" ID="ID_937983024" MODIFIED="1347809398528" TEXT="trzeba ostro&#x17c;nie pos&#x142;ugiwa&#x107; si&#x119; t&#x105; wiedz&#x105;"/>
+</node>
+<node CREATED="1347809404935" ID="ID_924159594" MODIFIED="1347809409128" TEXT="dwa sk&#x142;adniki sukcesu">
+<node CREATED="1347809425606" ID="ID_736832781" MODIFIED="1347809430366" TEXT="&#x107;wiczenia w terenie"/>
+<node CREATED="1347809465791" ID="ID_1164892540" MODIFIED="1347809489193" TEXT="wykszta&#x142;cenie odpowiednich cech">
+<node CREATED="1347809490868" ID="ID_1291345973" MODIFIED="1347809497654" TEXT="&lt;-- FUNDAMENT"/>
+</node>
+<node CREATED="1347809475972" ID="ID_1505173346" MODIFIED="1347809479597" TEXT="opanowanie technik"/>
+</node>
+<node CREATED="1347809847481" ID="ID_965447974" MODIFIED="1347809849379" TEXT="o co chodzi">
+<node CREATED="1347809860085" ID="ID_754590090" MODIFIED="1347809864635" TEXT="tego nikt nie jest &#x15b;wiadomy">
+<node CREATED="1347810051430" ID="ID_902262358" MODIFIED="1347810062180" TEXT="wst&#x119;pna ocena na podstawie wygl&#x105;du i zachowania"/>
+<node CREATED="1347810064796" ID="ID_727893775" MODIFIED="1347810306838" TEXT="ilo&#x15b;&#x107; po&#x15b;wi&#x119;canje uwagi i czasu">
+<node CREATED="1347810311324" ID="ID_677149096" MODIFIED="1347810322661" TEXT="zbyt ma&#x142;a = &#x179;LE"/>
+<node CREATED="1347810323380" ID="ID_390206457" MODIFIED="1347810330837" TEXT="zbyt du&#x17c;a = &#x179;LE"/>
+</node>
+<node CREATED="1347810354323" ID="ID_698885895" MODIFIED="1347810360229" TEXT="nie dawaj wszystkiego OD RAZU"/>
+<node CREATED="1347810361868" HGAP="21" ID="ID_978401803" MODIFIED="1347810445394" TEXT="pocz&#x105;tek ma by&#x107; subtelny, pe&#x142;en niedom&#xf3;wie&#x144;, z ograniczonym zaanga&#x17c;owaniem" VSHIFT="-6"/>
+</node>
+<node CREATED="1347810424176" ID="ID_863135412" MODIFIED="1347810428088" TEXT="jak dobieramy si&#x119; w pary">
+<node CREATED="1347810458630" ID="ID_1816508708" MODIFIED="1347810496348" TEXT="m&#x119;&#x17c;czy&#x17a;ni kieruj&#x105; si&#x119;">
+<node CREATED="1347810462697" ID="ID_1481192884" MODIFIED="1347810465986" TEXT="wygl&#x105;d"/>
+<node CREATED="1347810466580" ID="ID_1594410944" MODIFIED="1347810467771" TEXT="zdrowie"/>
+</node>
+<node CREATED="1347810480161" ID="ID_1537070260" MODIFIED="1347810499950" TEXT="kobiety kieruj&#x105; si&#x119;">
+<node CREATED="1347810512842" ID="ID_424756037" MODIFIED="1347810519389" TEXT="wygl&#x105;d mniej istotny"/>
+<node CREATED="1347810519747" ID="ID_1785923825" MODIFIED="1347810525978" TEXT="si&#x142;a - nie tylko fizyczna"/>
+<node CREATED="1347810526217" ID="ID_187762531" MODIFIED="1347811062682" TEXT="umiej&#x119;tno&#x15b;&#x107; zapewnienia ochrony i bezpiecze&#x144;stwa">
+<node CREATED="1347811067219" ID="ID_1573474046" MODIFIED="1347811070068" TEXT="finansowe"/>
+<node CREATED="1347811070329" ID="ID_1399647132" MODIFIED="1347811073223" TEXT="fizyczne"/>
+<node CREATED="1347811075511" ID="ID_130642542" MODIFIED="1347811079130" TEXT="wsparcie psychiczne"/>
+</node>
+</node>
+<node CREATED="1347811221928" ID="ID_1345388820" MODIFIED="1347811233232" TEXT="ca&#x142;o&#x15b;&#x107; odbywa si&#x119; w wi&#x119;kszo&#x15b;ci na poziomie pod&#x15b;wiadomym"/>
+<node CREATED="1347811541838" ID="ID_1487369275" MODIFIED="1347811594032" TEXT="rzucanie si&#x119; na drug&#x105; osob&#x119; bez zachamowa&#x144; i wymaga&#x144; &#x15b;wiadczy o desperacji, s&#x142;abo&#x15b;ci i nieatrakcyjno&#x15b;ci (bo atrakcyjne osoby maj&#x105; wyg&#xf3;r i nigdy tak nie reaguj&#x105;)"/>
+</node>
+</node>
+<node CREATED="1347811601727" ID="ID_823007220" MODIFIED="1347811609828" TEXT="klucz do sztuki uwodzenia">
+<node CREATED="1347811619850" ID="ID_1885401044" MODIFIED="1347811635390" TEXT="zademonstrowanie w&#x142;asnej warto&#x15b;ci musi si&#x119; odbywa&#x107; na poziomie pod&#x15b;wiadomym"/>
+<node CREATED="1347812401992" ID="ID_1950428983" MODIFIED="1347812437479" TEXT="rzu&#x107; wyzwanie i okazuj braki zainteresowania"/>
+</node>
+<node CREATED="1347812655195" ID="ID_895780468" MODIFIED="1347812674504" TEXT="najprostsza droga do jej serca">
+<node CREATED="1347812675891" ID="ID_125353035" MODIFIED="1347812684545" TEXT="spraw by to kobieta zdobywa&#x142;a ciebie"/>
+<node CREATED="1347812691635" ID="ID_255311556" MODIFIED="1347812704203" TEXT="musisz pokaza&#x107;, &#x17c;e jeste&#x15b; czym&#x15b; bardzo warto&#x15b;ciowym, co si&#x119; chce mie&#x107;"/>
+</node>
+</node>
 </node>
 </map>
